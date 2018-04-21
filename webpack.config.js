4359253ea828247158c41143fca94965fd4be8d6
Bin Xin <rhyzix@gmail.com>
2018-04-21 23:28:30 +0800

    skeleton

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const PROD = process.argv.includes('-p')
+const webpack = require('webpack')
+
+module.exports = {
+  context: `${__dirname}/src`,
+  resolve: {
+    modules: ['src', 'node_modules'],
+  },
+  entry: {
+    app: './app.js',
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: `${process.env.PUBLIC || ''}`,
+    filename: '[name].js',
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      // exclude: /node_modules/,
+      exclude: /node_modules\/(?!lib)/,
+      // use: ['babel-loader'],
+      use: {
+        loader: 'babel-loader',
+        options: {
+          // ignore babelrc in node_modules
+          babelrc: false,
+          presets: [ ['env', { modules: false }] ],
+          plugins: [
+            'transform-runtime',
+            'transform-object-rest-spread',
+            'transform-react-jsx'/*, { pragma: 'x' }*/,
+            ['transform-class-properties', { spec: true }],
+          ],
+        },
+      },
+    }, {
+      test: /\.(png|jpg|gif|mp4|mp3|m4a)$/,
+      loader: 'file-loader',
+      options: {
+        name: 'assets/[name]-[hash:8].[ext]',
+      },
+    }],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
+    }),
+  ],
+}
