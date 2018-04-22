ab19c3617dcc563f44cac16c32a36e92f8502be9
Bin Xin <rhyzix@gmail.com>
2018-04-22 15:31:54 +0800

    basic each

--- src/app.js
+++ src/app.js
@@ -4,42 +4,79 @@ window.compute = compute
 
 const toNode = (value) => {
   if (value instanceof Node) return value
-  if (typeof value === 'undefined') return document.createComment('')
-  // if (Array.isArray(value))
+  // if (typeof value === 'undefined') return document.createComment('')
   return document.createTextNode(`${value}`)
 }
+const toNodes = (value) => {
+  const values = Array.isArray(value) ? value : [value]
+  return values.map(toNode)
+}
+
+const getAndWatch = (value, callback) => {
+  const handle = () => callback(value())
+  // TODO watch first to prevent repeat compute() bug
+  value.onChange(handle)
+  handle()
+  return () => value.offChange(handle)
+}
+
 
 const x = (name, attributes, ...children) => {
   const el = document.createElement(name)
 
+  console.log(attributes)
   if (attributes !== null)
   for (const [key, value] of Object.entries(attributes)) {
-    el.setAttribute(key, value)
-    // TODO events
-    // TODO boolean attributes
-    // TODO props
-    // TODO reactive
+    // TODO props(by adapter?)
+    if (/^on/.test(key) && typeof value !== 'string') {
+      // once?
+      const eventName = key.slice(2)
+      el.addEventListener(eventName, value, false)
+    } else {
+      const set = v => {
+        if (typeof v === 'boolean') {
+          if (v) el.setAttribute(key, '')
+          else el.removeAttribute(key)
+        } else {
+          el.setAttribute(key, v)
+        }
+      }
+
+      if (typeof value === 'function') {
+        getAndWatch(value, set)
+      } else {
+        set(value)
+      }
+    }
   }
-  console.log(children)
+
   for (const child of children) {
     console.log(child)
     if (typeof child === 'function') {
-      // reactive
-      let node
-      child.onChange(() => {
-        const newValue = child()
-        const newNode = toNode(newValue)
-        // TODO? update textNode by textNode.data
-        el.replaceChild(newNode, node)
-        node = newNode
-      })
 
-      // watch first to prevent repeat compute() bug
-      const value = child()
-      node = toNode(value)
-      el.appendChild(node)
+      const anchor = document.createComment('')
+      el.appendChild(anchor)
+      // startAnchor/endAnchor and update by remove items between them?
+
+      let nodes = []
+      // TODO offChange
+      getAndWatch(child, v => {
+        const newNodes = toNodes(v)
+        // TODO? efficient update
+        // textNode by textNode.data
+        // node by replaceChild
+        for (const node of nodes) {
+          el.removeChild(node)
+        }
+        nodes = newNodes
+        for (const node of nodes) {
+          el.insertBefore(node, anchor)
+        }
+      })
     } else {
-      el.appendChild(toNode(child))
+      for (const node of toNodes(child)) {
+        el.appendChild(node)
+      }
     }
   }
 
@@ -49,6 +86,7 @@ const x = (name, attributes, ...children) => {
 // TODO normal value
 const noop = () => {}
 x.if = (value, truthy, falsy=noop) => compute(() => value() ? truthy() : falsy())
+// TODO cache and reuse
 x.each = (items, mapper, empty=noop) => compute(() => items().map(mapper))
 
 
@@ -72,23 +110,41 @@ x.each = (items, mapper, empty=noop) => compute(() => items().map(mapper))
 // $("#total").text() === "5"
 
 const firstName = compute.value('Rick')
+const lastName = compute.value('Nope')
 const isOk = compute.value(true)
 const items = compute.value(['itemA', 'itemB'])
 
 window.firstName = firstName
+window.lastName = lastName
 window.isOk = isOk
 window.items = items
 
+const fullName = compute(() => `${firstName()} ${lastName()}`)
+
+const clickCount = compute.value(0)
+
 const out = <div>
-  <h1>{firstName}</h1>
+  <h1 title={fullName}>{firstName}</h1>
+  <h2>{fullName}</h2>
   {x.if(isOk, () =>
     <div>ok</div>
   )}
-  <ul>
-  {x.each(items, (item) =>
-    <li>{item}</li>
-  )}
+  <ul onclick="console.log(2)">
+    {[<li>first</li>, <li>second</li>]}
+    {x.each(items, (item) =>
+      <li>{item}</li>
+    )}
+    <li>last</li>
   </ul>
+  <span hidden>s</span>
+  <button type="button" onclick={() => clickCount.set(clickCount() + 1)}>Click Me! {clickCount}</button>
 </div>
 
 document.body.appendChild(out)
+
+class Foo {
+  get name() {
+    return '123'
+  }
+}
+window.foo = new Foo
