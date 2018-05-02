b090d596f0ce525f9df7d5bc221d74f7798a7d91
Bin Xin <rhyzix@gmail.com>
2018-05-02 17:42:11 +0800

    functional jsx

--- jsx.js
+++ jsx.js
@@ -1,5 +1,7 @@
-// todo transform-jsx-functional
-const jsx = require('babel-plugin-transform-react-jsx')
+// transform-jsx-functional
+const jsx = require('babel-plugin-syntax-jsx')
+const helper = require('babel-helper-builder-react-jsx')
+// const react = require('babel-plugin-transform-react-jsx')
 
 module.exports = ({ types: t }) => {
   const JSXSpreadChild = (path) => {
@@ -8,10 +10,18 @@ module.exports = ({ types: t }) => {
     )
   }
 
+  const visitor = helper({
+    post(state, pass) {
+      // TODO @jsx-lowercase-create
+      // https://github.com/babel/babel/blob/v6.24.1/packages/babel-plugin-transform-react-jsx/src/index.js#L19
+      const { tagExpr } = state
+      state.callee = tagExpr
+    },
+  })
+  visitor.JSXSpreadChild = JSXSpreadChild
+
   return {
     inherits: jsx,
-    visitor: {
-      JSXSpreadChild,
-    },
+    visitor,
   }
 }
