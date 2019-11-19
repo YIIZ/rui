c242e6f0c59df691ac1e7bed4c12f700d4f057e8
Bin Xin <rhyzix@gmail.com>
2019-11-20 02:40:12 +0800

    caret tip

--- rui/pixi/Input.js
+++ rui/pixi/Input.js
@@ -1,15 +1,38 @@
 // @jsx h
 import { h, value, compute, hook, if as _if, each } from 'rui'
-import { Text, Container } from './nodes'
+import { Sprite, Text, Container } from './nodes'
 
 import DOMDummy from './DOMDummy'
+import * as PIXI from 'pixi.js'
+import { tween } from 'popmotion'
+import * as easing from '@popmotion/easing'
 
+function Caret(props) {
+  const [alpha, setAlpha] = value(1)
+  hook(() => {
+    const play = tween({
+      from: 0.8, to: 0.2, duration: 800,
+      yoyo: Infinity,
+      ease: easing.linear,
+    }).start(setAlpha)
+    return () => play.stop()
+  })
+  return <Sprite tex={PIXI.Texture.WHITE} alpha={alpha}
+    width={4}
+    {...props}
+  />
+}
 
 export default function Input({ placeholder, ratio, fontSize, fill, fontFamily, ...props }, children) {
-  const [text, setText] = value(placeholder)
+  const [text, setText] = value('')
+  const usePlaceholder = compute(() => text().length < 1)
+  const displayText = compute(() => usePlaceholder() ? placeholder : text())
 
   // select all on focus https://stackoverflow.com/a/4067488
   const selectAllText = (el) => el.setSelectionRange(0, el.value.length)
+
+  const textNode = <Text fontSize={fontSize} fill={fill} fontFamily={fontFamily}>{displayText}</Text>
+  // const caret = <Sprite tex={PIXI.Texture.WHITE} />
 
   const node = <DOMDummy tag="input"
     style="font-size: 1px;" ratio={ratio}
@@ -18,7 +41,8 @@ export default function Input({ placeholder, ratio, fontSize, fill, fontFamily, 
   >
     <Container {...props}>
       {...children}
-      <Text fontSize={fontSize} fill={fill} fontFamily={fontFamily}>{text}</Text>
+      {_if(usePlaceholder, () => <Caret x={textNode.el.width*-0.5-10} height={textNode.el.height} />)}
+      {textNode}
     </Container>
   </DOMDummy>
 
