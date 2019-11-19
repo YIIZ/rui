605c8234d7033f7bb6273cebc846a1aa24b0ffa7
Bin Xin <rhyzix@gmail.com>
2019-11-20 02:06:31 +0800

    update spine confs

--- rui/pixi/Spine.js
+++ rui/pixi/Spine.js
@@ -1,16 +1,30 @@
+// @jsx h
+import { h, value, compute, hook } from 'rui'
 import { Node } from './nodes'
-import { Spine } from 'val-loader!./spine.js.val'
+import { Spine } from '!!val-loader!./spine.js.val'
 
+// TODO tracks?
 export default function ({
-  data, animation, loop=false,
+  data, animation, loop=false, animations,
   audios, skin, onAnimationEnd, onAudio,
+  timeScale = 1,
   ...props
 }, children) {
   const el = new Spine(data)
-  const { slotContainers, skeleton, state } = el
+  const { slotContainers, skeleton, state, stateData } = el
+
+  stateData.defaultMix = 0.1
+  state.timeScale = timeScale
 
   if (animation) {
     state.setAnimation(0, animation, loop)
+  }
+  if (animations) {
+    animations.forEach((a, i) => {
+      const last = i === animations.length-1
+      const loop = last
+      state.addAnimation(0, a, loop, 0)
+    })
   }
   if (onAnimationEnd) {
     state.addListener({ complete: onAnimationEnd })
@@ -32,12 +46,12 @@ export default function ({
   }
   // TODO skin
 
-  if (children) for (const { slot, node, ...props } of children) {
+  if (children) for (const { slot, node, clear, ...props } of children) {
     const container = slotContainers[skeleton.findSlotIndex(slot)].children[0]
     const containerNode = node
       ? <Node el={container} {...props}>{node}</Node>
       : <Node el={container} {...props}></Node>
-    if (node) {
+    if (clear) {
       container.texture = null // clear texture if children exits
     }
     hook(() => {
@@ -46,5 +60,14 @@ export default function ({
       return () => containerNode.detach()
     })
   }
-  return <Node el={el} {...props}></Node>
+
+  const node = <Node el={el} {...props}></Node>
+
+  node.play = (name, timeScale=1) => {
+    const entry = el.state.setAnimation(0, name)
+    entry.timeScale = timeScale
+    return new Promise(complete => el.state.addListener({ complete }))
+  }
+
+  return node
 }
