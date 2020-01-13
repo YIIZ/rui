862bc1333c3affce42c7d8b43bb6d13ca92271ac
Bin Xin <rhyzix@gmail.com>
2020-01-13 17:12:07 +0800

    fix compatibility

--- rui/pixi/Closable.js
+++ rui/pixi/Closable.js
@@ -1,10 +1,10 @@
 // @jsx h
 import * as PIXI from 'pixi.js'
-import { h, value, compute, if as _if } from 'rui'
+import { h, value, compute, replace } from 'rui'
 import { Container } from './nodes'
 
 export default function Closable({ open, ...props }, children) {
   return <Container visible={open} {...props}>
-    {_if(open, () => children)}
+    {replace(() => open() ? children : null)}
   </Container>
 }
