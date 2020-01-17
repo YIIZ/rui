229d1a87fec5998e1fe51fbabd3488e670420e32
Bin Xin <rhyzix@gmail.com>
2020-01-17 19:22:02 +0800

    video time

--- rui/pixi/Video.js
+++ rui/pixi/Video.js
@@ -6,7 +6,11 @@ import { everyFrame } from 'popmotion'
 
 import DOMDummy from './DOMDummy'
 
-export default function Video({ src, onEnd, onTimeUpdate, width, height, ...props }) {
+export default function Video({ src, onEnd, width, height, ...props }) {
+  const [time, setTime] = value(0)
+  const [duration, setDuration] = value(Infinity)
+  const remain = compute(() => duration() - time())
+
   // Container has no size
   const node = <Container {...props}><Sprite width={width} height={height}></Sprite></Container>
 
@@ -18,13 +22,16 @@ export default function Video({ src, onEnd, onTimeUpdate, width, height, ...prop
     playsinline
     webkit-playsinline
     onended={onEnd}
-    ontimeupdate={onTimeUpdate}
+    ondurationchange={() => setDuration(dom.duration)}
+    ontimeupdate={() => setTime(dom.currentTime)}
   >
     {node}
   </DOMDummy>
-
   const { dom } = dummy
 
+  dummy.time = time
+  dummy.duration = duration
+  dummy.remain = remain
   dummy.play = async () => {
     dom.play()
     await new Promise((resolve) => {
