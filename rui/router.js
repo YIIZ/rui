425bc74a60559de91ca1497aed220d40a66b3942
Bin Xin <rhyzix@gmail.com>
2020-07-12 17:50:55 +0800

    add todo

--- rui/router.js
+++ rui/router.js
@@ -2,6 +2,9 @@
 import { h, take, value, compute, peek, if as _if } from 'rui'
 import { sep, join, normalize } from 'path'
 
+// TODO
+// history?
+// go(path, state)?
 const make = (segments, onRoute, depth=0, dir='/') => {
   // base, ext?
   // https://nodejs.org/api/path.html#path_path_parse_path
@@ -30,3 +33,5 @@ export function useRouter(path_, onRoute_) {
   const segments = compute(() => normalize(`/${path()}/`).split(sep).slice(1, -1))
   return make(segments, onRoute)
 }
+
+export default useRouter
