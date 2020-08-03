a63749d3b8ca9007212f904236671143c01b523a
Bin Xin <rhyzix@gmail.com>
2020-08-04 01:54:58 +0800

    parse

--- rui/router.js
+++ rui/router.js
@@ -3,12 +3,22 @@ import { h, take, value, compute, peek, if as _if } from 'rui'
 import { join, normalize as normalize_ } from 'path'
 
 export const normalize = p => normalize_(`/${p}`).slice(1) // remove leading `/`
-export const slice = (path, name) => {
+
+export const parse = (path) => {
   // TODO shared current?
   const normalized = compute(() => normalize(path()))
   const sepIndex = compute(() => normalized().indexOf('/'))
   const sepExists = compute(() => sepIndex() >= 0)
   const current = compute(() => sepExists() ? normalized().slice(0, sepIndex()) : normalized())
+  return {
+    normalized,
+    current,
+    sepIndex,
+    sepExists,
+  }
+}
+export const slice = (path, name) => {
+  const { current, sepExists, sepIndex, normalized } = parse(path)
 
   const match = compute(() => current() === name)
   // persist while transition
